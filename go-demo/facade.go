917ea55dc24351f51284c30d7d442d97dd092dce
translucens <mail@w-st.com>
2019-08-07 11:43:10 +0900

    Go版デモ

---
+++ go-demo/facade.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+const (
+	backendURL = "http://backend-1:8080"
+)
+
+func main() {
+
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		log.Printf("invalid port specified: %v", err)
+		return
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := http.Get(backendURL)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("failed to get a content from backend: %v", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		w.WriteHeader(http.StatusOK)
+		bytes, err := io.Copy(w, resp.Body)
+		if err != nil {
+			log.Printf("failed to send a response: %v", err)
+			return
+		}
+		log.Printf("sent response: %d bytes", bytes)
+	})
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
---
+++ go-demo/go.mod
@@ -0,0 +1,3 @@
+module github.com/translucens/telepresence-demo/go-demo
+
+go 1.12
